a4b5d6f83278e2d9fc0c29fb1b38eb253c0c48be
Canklot <mustafacandan211@gmail.com>
2022-08-23 15:09:23 +0300

    First working qr generator version

--- qr_project/qr_app/views.py
+++ qr_project/qr_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect,FileResponse
+from .forms import TextForm
+import qrcode
+from PIL import Image
+
 
 mylist = [
     {
@@ -23,3 +27,26 @@ def index(request):
     
 def bathroom(request):
     return render(request,"qr_app/bathroom.html",{"title":"dirty"})
+
+def form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TextForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            img = qrcode.make(form.cleaned_data['text'])
+            response = HttpResponse(content_type='image/png')
+            response['Content-Disposition'] = 'attachment; filename="output.png"'
+            img.save(response,"PNG")
+            return response
+           # return FileResponse(form.cleaned_data['text'])
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TextForm()
+
+    return render(request, 'qr_app/form.html', {'form': form})
