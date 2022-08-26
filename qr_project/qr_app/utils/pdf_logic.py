2c69fc8a247ac04eb89825e8487e9c7ddb455b75
Canklot <mustafacandan211@gmail.com>
2022-08-26 17:34:46 +0300

    PDF logic is improved

--- qr_project/qr_app/utils/pdf_logic.py
+++ qr_project/qr_app/utils/pdf_logic.py
@@ -2,19 +2,16 @@ from io import BytesIO
 import fitz
 
 def convert_to_pdf(imglist):
-    doc = fitz.open()  # PDF with the pictures
+    doc = fitz.open()  # empty output PDF
+    paper_height, paper_width = fitz.paper_size("A6")
     for file in imglist:
-        # open pic as document. Convert this to use with
-        img = fitz.open("jpg", file)
-        rect = img[0].rect  # pic dimension
-        pdfbytes = img.convert_to_pdf()  # make a PDF stream
-        img.close()  # no longer needed. Remove after with
-        imgPDF = fitz.open("pdf", pdfbytes)  # open stream as PDF
-        paper_height, paper_width = fitz.paper_size("A6")
-        # new page with ...# pic dimension
+        with fitz.open("jpg", file) as img  :
+            rect = img[0].rect  # pic dimension
         page = doc.new_page(width=paper_width, height=paper_height)
-        page.show_pdf_page(rect, imgPDF, 0)  # image fills the page
+        page.insert_image(rect, stream=file) 
+        #rect where to place the image on current page. 
     binary_pdf = BytesIO()
     doc.save(binary_pdf)
     binary_pdf = binary_pdf.getvalue()
+    # remove any BytesIO class variables except content
     return binary_pdf
