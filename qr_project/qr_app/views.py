9002916e4e1c349361a72ecd6a17aa1830637791
Canklot <mustafacandan211@gmail.com>
2022-08-26 13:25:13 +0300

    Added not working api

--- qr_project/qr_app/views.py
+++ qr_project/qr_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, FileResponse
 from .utils.barcode_logic import convert_list_to_barcode, convert_list_to_qr
 from .forms import TextForm
+from django.views.decorators.csrf import csrf_exempt
+
 
 mylist = [
     {
@@ -29,7 +31,23 @@ def bathroom(request):
         return HttpResponse("You only see this mesage when debug detected")
     return render(request, "qr_app/bathroom.html", {"title": "dirty"})
 
-
+#This api part is probably bad design I didnt followed any tutorial. Might be bad design. Doesnt work btw
+@csrf_exempt
+def api(request):
+    text = request.GET.get('text', '')
+    barcode_type = request.GET.get('barcode_type', '')
+    lines = text.split(",")
+    
+    if barcode_type == "qr_code":
+        pdf = convert_list_to_qr(lines)
+    elif barcode_type == "barcode_code128":
+        pdf = convert_list_to_barcode(lines)
+    else:
+        return HttpResponse("Unkown barcode_type")
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="output.pdf"'
+    return response
+        
 def form(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
