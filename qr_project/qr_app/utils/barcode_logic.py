5d1fff20903ba36938040eb19884cd7685dccbf0
Canklot <mustafacandan211@gmail.com>
2022-08-29 12:27:42 +0300

    changed convert_to_pdf name to img_list_to_pdf

--- qr_project/qr_app/utils/barcode_logic.py
+++ qr_project/qr_app/utils/barcode_logic.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 import qrcode
-from .pdf_logic import convert_to_pdf
+from .pdf_logic import img_list_to_pdf
 
 from barcode import Code128
 from barcode.writer import ImageWriter
@@ -13,14 +13,15 @@ def convert_list_to_qr(lines):
     qr_images = []
     for line in lines:
         img = qrcode.make(line,version=2)
+        # version 2	is 25x25 and supports 47 chars
         font_type = ImageFont.truetype("calibri.ttf", 24)
         ImageDraw.Draw(img).text((40, 300), line, font=font_type)
-        # Writes text under the QR code
+        # writes text under the QR code
         fomatted_img = BytesIO()
         img.save(fomatted_img, format="JPEG")
         fomatted_img = fomatted_img.getvalue()
         qr_images.append(fomatted_img)
-    pdf = convert_to_pdf(qr_images)
+    pdf = img_list_to_pdf(qr_images)
     return pdf
 
 
@@ -32,5 +33,5 @@ def convert_list_to_barcode(lines):
             barcode_image, {"font_size": 6, 
                             "text_distance": 2})
         barcode_image_list.append(barcode_image)
-    pdf = convert_to_pdf(barcode_image_list)
+    pdf = img_list_to_pdf(barcode_image_list)
     return pdf
