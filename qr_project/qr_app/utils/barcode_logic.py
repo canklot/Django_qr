1c64f0ecaae9365fa8d1b515de4eb378a271e71a
Canklot <mustafacandan211@gmail.com>
2022-08-31 13:42:47 +0300

    Added quality option to save jpeg

--- qr_project/qr_app/utils/barcode_logic.py
+++ qr_project/qr_app/utils/barcode_logic.py
@@ -16,7 +16,7 @@ def convert_list_to_qr(lines):
         ImageDraw.Draw(img).text((40, 300), line, font=font_type)
         # writes text under the QR code
         fomatted_img = BytesIO()
-        img.save(fomatted_img, format="JPEG")
+        img.save(fomatted_img, format="JPEG",quality=75)
         fomatted_img = fomatted_img.getvalue()
         qr_images.append(fomatted_img)
     pdf = img_list_to_pdf(qr_images)
@@ -27,7 +27,7 @@ def convert_list_to_barcode(lines):
     barcode_image_list = []
     for line in lines:
         barcode_image = BytesIO()
-        Code128(line, writer=ImageWriter()).write(
+        Code128(line, writer=ImageWriter(format="JPEG")).write(
             barcode_image, {"font_size": 6,
                             "text_distance": 2})
         barcode_image_list.append(barcode_image)
