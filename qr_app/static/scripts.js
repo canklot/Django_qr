b227733fa14887149cbd246f86a1ad79d94a3abd
Canklot <mustafacandan211@gmail.com>
2022-09-20 13:53:37 +0300

    feth file download version 1

--- qr_app/static/scripts.js
+++ qr_app/static/scripts.js
@@ -20,5 +20,46 @@ $("#qr_form_id").on("submit", function () {
       return false;
     }
   }
+  myFetch();
   return true;
 });
+
+function myFetch() {
+  fetch("http://localhost:8000/api", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text: ["asd1", "asd2"],
+      barcode_type_selection: "qr_code",
+    }),
+  })
+    .then((response) => {
+      return response.blob();
+    })
+    .then((blob) => {
+      var reader = new FileReader();
+      reader.readAsArrayBuffer(blob);
+      reader.onloadend = function () {
+        var theBuffer = reader.result;
+        downloadFile(theBuffer);
+      };
+    });
+}
+
+//------------------
+function downloadFile(data, name = "file.pdf") {
+  const blob = new Blob([data], { type: "octet-stream" });
+  const href = URL.createObjectURL(blob);
+  const a = Object.assign(document.createElement("a"), {
+    href,
+    style: "display:none",
+    download: name,
+  });
+  document.body.appendChild(a);
+  a.click();
+  URL.revokeObjectURL(href);
+  a.remove();
+}
