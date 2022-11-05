703688bd6c10010cd93e5840b82fc45c727cbd31
Canklot <mustafacandan211@gmail.com>
2022-11-05 13:54:10 +0300

    api auth added

--- qr_app/urls.py
+++ qr_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('api', views.api, name='api'),
     path('api_usage', views.api_usage, name='api_usage'),
     path('sitemap.xml/', views.sitemap, name='sitemap'),
+    path('register', views.RegisterView.as_view()),
+    path('login', views.LoginView.as_view()),
+    path('user', views.UserView.as_view()),
+    path('logout', views.LogoutView.as_view()),
 ]
