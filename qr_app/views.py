020aef5d359f7b3317f0445b817fcf06c2e22e25
Canklot <mustafacandan211@gmail.com>
2022-11-12 16:43:52 +0300

    check if message exist

--- qr_app/views.py
+++ qr_app/views.py
@@ -7,6 +7,8 @@ import requests
 from .forms import TextForm
 from .utils.pipeline_pdf import pipeline_pdf
 
+jsonlist = []
+
 
 def index(request):
     if request.method != 'POST':
@@ -82,8 +84,11 @@ def webhook(request):
     if request.method == "POST":
         if request.headers.get('content-type') == 'application/json':
             json_data = json.loads(request.body)
-            print(json_data["entry"][0]["changes"][0]
-                  ["value"]["messages"][0]["text"]["body"])
+
+            with open("jsonlogs.json", 'a', encoding='utf-8') as f:
+                f.writelines(json.dumps(json_data) + "\n")
+
+            
 
         temp_ac_token = "EAAGZBco97Pm8BAIm03Y760Bp27orQjgGNLTuRo3hAcb3tq4YcImZB9CwrrfOSq4EqWFz0ZB4EqjlZBBZAvsTiitpjV2gLInAcvJeZB6vSIZBeFNMtoMX2l5JGAnYlARcx0CuclNFbjFCw4mVHj4znWTABRKxZBPxqCnk733d1Qonk9Dlvb7B4EZAwgopYgDtcOBZBl3KhwGnZAGRgZDZD"
 
@@ -104,10 +109,17 @@ def webhook(request):
             "template": my_template
         }
 
-        json_to_send = json.dumps(my_data)
-        response = requests.post(
-            post_url, data=json_to_send, headers=my_headers)
-        content = response.content
-        print(content)
+        jsonlist.append(json_data)
+        if "messages" in json_data["entry"][0]["changes"][0]["value"]:
+                message_recived = json_data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"]
+                print(message_recived)
+        else:
+            message_recived = None
 
-    return HttpResponse("end")
+        if message_recived == "hi":
+            json_to_send = json.dumps(my_data)
+            response = requests.post(post_url, data=json_to_send, headers=my_headers)
+            content = response.content
+            print(content)
+    # You need to return blank 200 ok mesage to sender to say I revieved the message stop sending it
+    return HttpResponse("")
