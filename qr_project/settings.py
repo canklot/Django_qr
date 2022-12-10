b0a6993b2c4592e8461039ef4628a8c41d271ca6
Canklot <mustafacandan211@gmail.com>
2022-12-10 18:59:39 +0300

    www.createbarcode.tk

--- qr_project/settings.py
+++ qr_project/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = 'django-insecure-9x6a4iu*7z70jq@mpr$mh_8)w&ijv3@jbjui&zn_+&-gasvk$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', 'www.canklottools.tk',
-                 'www.canklotsoftware.tk', 'createbarcode.tk', '.eu.ngrok.io']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', 
+                'www.canklottools.tk', 'www.canklotsoftware.tk', 
+                'createbarcode.tk', 'www.createbarcode.tk', 
+                '.eu.ngrok.io']
 
 
 # Application definition
