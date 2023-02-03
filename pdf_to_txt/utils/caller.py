d9b78c31ab2014ef15b55f6d6554e13ff70c0636
Canklot <mustafacandan211@gmail.com>
2023-02-03 11:43:51 +0300

    fitzclimc modified to use from other script

---
+++ pdf_to_txt/utils/caller.py
@@ -0,0 +1,3 @@
+from fitzclimc import gettext
+
+gettext("C:/Users/Canavar/Desktop/Projeler/Django_qr/pdf_to_txt/utils/3page.pdf" ,"C:/Users/Canavar/Desktop/Projeler/Django_qr/pdf_to_txt/utils/output.txt")
--- pdf_to_txt/views.py
+++ pdf_to_txt/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UploadFileForm
+import fitz
+from .utils.fitzclimc import gettext
+import io
+
 
 # Create your views here.
 def index(request):
-    if request.method != 'POST':
-        return HttpResponse("asd")
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse('the text is: ')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+ 
+def handle_uploaded_file(file_to_handle):
+    memory_file = io.BytesIO()
+    
