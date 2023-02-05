ddef29387f8d2dac43cb6a5720e35a73b8772630
Canklot <mustafacandan211@gmail.com>
2023-02-05 21:58:00 +0300

    auto submit

--- pdf_to_txt/forms.py
+++ pdf_to_txt/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(widget=forms.FileInput(attrs={'id': 'file_selector_id' ,'onchange':'form.submit()'}))
